29ef5d78700602627921be744a494c95c271eb7c
Luigi <luigiiazzetta98@gmail.com>
2020-11-18 15:38:09 +0100

    Added requirements.txt

--- test_2.py
+++ test_2.py
@@ -1,8 +1,44 @@
+import logging
+
+filename = 'log.txt'
+
+# create logger
+logger = logging.getLogger('LogExample')
+logger.setLevel(logging.DEBUG)
+
+# create file
+file = logging.FileHandler(filename)
+file.setLevel(logging.DEBUG)
+logger.addHandler(file)
+
+def istriangle(l1, l2, l3):
+    if l1 + l2 < l3:
+        logger.error("ERROR, third number is bigger than the sum other two")
+        return True
+    if l1 + l3 < l2:
+        logger.error("ERROR, second number is bigger than the sum other two")
+        return True
+    if l2 + l3 < l1:
+        logger.error("ERROR, first number is bigger than the sum other two")
+        return True
+
+    return False
+
+
 def test_method():
     first = input()
+    if not first.isdigit():
+        logger.error('ERROR, it must be a digit!')
     assert first.isdigit() == True
     second = input()
+    if not second.isdigit():
+        logger.error('ERROR, it must be a digit!')
     assert second.isdigit() == True
     third = input()
+    if not third.isdigit():
+        logger.error('ERROR, it must be a digit!')
     assert third.isdigit() == True
-    assert (first + second < third or first + third < second or second + third < first) == False
+
+    logger.debug("All 3 numbers are valid for testing")
+
+    assert istriangle(first, second, third) == False
