f6397dae8e91888e5e28fe6acf6a9c3bd2010656
Luigi <luigiiazzetta98@gmail.com>
2020-11-21 00:45:39 +0100

    Added real test, file gitignore and doc update

--- test_2.py
+++ test_2.py
@@ -6,39 +6,25 @@ filename = 'log.txt'
 logger = logging.getLogger('LogExample')
 logger.setLevel(logging.DEBUG)
 
-# create file
-file = logging.FileHandler(filename)
-file.setLevel(logging.DEBUG)
-logger.addHandler(file)
 
 def istriangle(l1, l2, l3):
-    if l1 + l2 < l3:
-        logger.error("ERROR, third number is bigger than the sum other two")
-        return True
-    if l1 + l3 < l2:
-        logger.error("ERROR, second number is bigger than the sum other two")
-        return True
-    if l2 + l3 < l1:
-        logger.error("ERROR, first number is bigger than the sum other two")
-        return True
+    if isinstance(l1, str) or isinstance(l2, str) or isinstance(l3, str):
+        return False
 
-    return False
+    if l1 <= 0 or l2 <= 0 or l3 <= 0:
+        return False
+
+    if (l1 + l2) <= l3 or (l1 + l3) <= l2 or (l2 + l3) <= l1:
+        return False
+
+    return True
 
 
 def test_method():
-    first = input()
-    if not first.isdigit():
-        logger.error('ERROR, it must be a digit!')
-    assert first.isdigit() == True
-    second = input()
-    if not second.isdigit():
-        logger.error('ERROR, it must be a digit!')
-    assert second.isdigit() == True
-    third = input()
-    if not third.isdigit():
-        logger.error('ERROR, it must be a digit!')
-    assert third.isdigit() == True
-
-    logger.debug("All 3 numbers are valid for testing")
-
-    assert istriangle(first, second, third) == False
+    assert istriangle(3, 4, 7) == False  # Should return False, this is NOT a Triangle
+    assert istriangle(5, 5, 5) == True  # Should return True, this is a Triangle
+    assert istriangle(0, 0, 0) == False  # Should return False, can't be a Triangle
+    assert istriangle('ciao', 4, 7) == False  # Should return False, passing a string
+    assert istriangle(-2, 1, 1) == False  # Should return False, one side is negative
+    assert istriangle(2, 1, -1) == False  # Should return False, one side is negative
+    assert istriangle(-5, -5, -5) == False  # Should return False, all 3 numbers are negative
